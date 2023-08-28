75e78ce39d803ba317e4166fcb6a8b0cc21a33d1
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-08-28 17:59:43 -0600

    21. AutoMapper

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
@@ -12,10 +13,12 @@ namespace Mango.Services.CouponAPI.Controllers
     {
         private readonly AppDbContext _db;
         private ResponseDto _responseDto;
+        private IMapper _mapper;
 
-        public CouponAPIController(AppDbContext dbContext)
+        public CouponAPIController(AppDbContext dbContext, IMapper mapper)
         {
             _db = dbContext;
+            _mapper = mapper;
             _responseDto = new ResponseDto();
         }
 
@@ -25,7 +28,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 IEnumerable<Coupon> objList = _db.Coupons.ToList();
-                _responseDto.Result = objList;
+                _responseDto.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
             }
             catch (Exception ex)
             {
@@ -43,7 +46,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 var coupon = _db.Coupons.First(u => u.CouponId == id);
-                _responseDto.Result = coupon;
+                _responseDto.Result = _mapper.Map<CouponDto>(coupon); ;
             }
             catch (Exception ex)
             {
