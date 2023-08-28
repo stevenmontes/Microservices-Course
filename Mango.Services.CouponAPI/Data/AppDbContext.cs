773ab42e94f2b4495e736cce298b7422603b6089
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-08-28 17:34:52 -0600

    18. Seed Database

--- Mango.Services.CouponAPI/Data/AppDbContext.cs
+++ Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -7,9 +7,28 @@ namespace Mango.Services.CouponAPI.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode = "1DT23D",
+                DiscountAmount = 10,
+                MinAmount = 20,
+            });
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "A25FG",
+                DiscountAmount = 20,
+                MinAmount = 40,
+            });
+        }
     }
 }
