8b409dc57f4a8140c8cf0b7347d67849ab4ec04b
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-08-28 17:42:10 -0600

    19. Get all and Get Coupon by ID

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,52 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                var coupon = _db.Coupons.First(u => u.CouponId == id);
+                return coupon;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
