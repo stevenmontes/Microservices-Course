52601af2ba20a3a7d7a8e5eac2ee083b960f37cf
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-08-29 18:54:39 -0600

    28. Register Services in Program Class File

--- Mango.Web/Utility/StaticDetails.cs
+++ Mango.Web/Utility/StaticDetails.cs
@@ -2,6 +2,8 @@
 {
     public class StaticDetails
     {
+        public static string CouponAPIBase { get; set; } = string.Empty;
+
         public enum ApiType
         {
             GET, POST, PUT, DELETE,
