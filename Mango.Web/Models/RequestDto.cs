28b7ffad010bdb597dba8e310a3ae75e4195ac47
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-08-29 18:22:46 -0600

    24. Create Request and Response DTO

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,12 @@
+﻿using static Mango.Web.Utility.StaticDetails;
+
+namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; } = string.Empty;
+        public object Data { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+    }
+}
---
+++ Mango.Web/Utility/StaticDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Utility
+{
+    public class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET, POST, PUT, DELETE,
+        }
+    }
+}
