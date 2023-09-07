31c29efc5f95144aa57edf24c665939f05f76643
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-06 18:17:55 -0600

    43. Add DTO's

---
+++ Mango.Services.AuthAPI/Models/Dto/RegisterationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class RegisterationRequestDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Mango.Services.AuthAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccessful { get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+    }
+}
