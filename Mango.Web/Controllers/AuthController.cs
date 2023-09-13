a8bdde3e0a8434b9b8adfab68a5eab3070d64ddc
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-13 11:12:05 -0600

    56. Dropdown for Role

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
@@ -23,6 +25,13 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>
+            {
+                new SelectListItem{Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin},
+                new SelectListItem{Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer}
+            };
+
+            ViewBag.RoleList = roleList;
             return View();
         }
 
--- Mango.Web/Utility/StaticDetails.cs
+++ Mango.Web/Utility/StaticDetails.cs
@@ -5,6 +5,8 @@
         public static string CouponAPIBase { get; set; } = string.Empty;
 
         public static string AuthAPIBase { get; set;} = string.Empty;
+        public static string RoleAdmin = "ADMIN";
+        public static string RoleCustomer = "CUSTOMER";
 
         public enum ApiType
         {
