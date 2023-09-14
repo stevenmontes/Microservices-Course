4675552cdd428315dfe14a1bcf16bcd80a42dfa0
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-14 14:01:08 -0600

    69. Product API Base Setup

---
+++ Mango.Services.ProductAPI/Models/Dto/ProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ProductAPI.Models.Dto
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.;
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
