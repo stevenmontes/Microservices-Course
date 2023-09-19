cae836c94a2ae221187191be972f32c2ab0343ea
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-19 13:59:02 -0600

    77. Details UI

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,21 +1,54 @@
 ﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto>? productDtos = new();
+            ResponseDto? response = await _productService.GetProductAsync();
+
+            if (response != null && response.IsSuccessful)
+            {
+                productDtos = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result)!);
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
+            return View(productDtos);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ProductDto? model = new();
+            ResponseDto? response = await _productService.GetProductAsync(productId);
+
+            if (response != null && response.IsSuccessful)
+            {
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result)!);
+            }
+            else
+            {
+                TempData["error"] = response.Message;
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
