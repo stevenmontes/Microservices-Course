a922e004c98d6f6bf031c6b3170e9e7537b443f7
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-22 16:20:07 -0600

    83. Cart Upsert Logic Part 1

--- Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -24,7 +26,46 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> Upsert(CartDto cartDto)
         {
+            try
+            {
+                var cartHeaderFromDb = await _appDbContext.CartHeaders.FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
 
+                if (cartHeaderFromDb == null)
+                {
+                    // Create Header and Details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    _appDbContext.CartHeaders.Add(cartHeader);
+                    await _appDbContext.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.CartHeaderId;
+                    CartDetails cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
+                    _appDbContext.CartDetails.Add(cartDetails);
+                    await _appDbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    // Check if details has same product
+                    var cartDetailsFromDb = await _appDbContext.CartDetails.FirstOrDefaultAsync(
+                        u => u.ProductId == cartDto.CartDetails.First().ProductId &&
+                        u.CartHeaderId == cartHeaderFromDb.CartHeaderId);
+
+                    if (cartDetailsFromDb == null)
+                    {
+                        // Create CartDetails
+                    }
+                    else
+                    {
+                        // Update count in Cart Details
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _responseDto.Message = ex.Message;
+                _responseDto.IsSuccessful = false;
+            }
+
+            return _responseDto;
         }
     }
 }
