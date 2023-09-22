a9386962ef3db69fee61ebcf0149b01344531756
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-22 15:55:58 -0600

    82. Create Cart API Controller

---
+++ Mango.Services.ShoppingCartAPI/Controllers/ShoppingCartAPIController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class ShoppingCartAPIController : ControllerBase
+    {
+        private ResponseDto _responseDto;
+        private IMapper _mapper;
+        private readonly AppDbContext _appDbContext;
+
+        public ShoppingCartAPIController(AppDbContext db, IMapper mapper)
+        {
+            _appDbContext = db;
+            _mapper = mapper;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> Upsert(CartDto cartDto)
+        {
+
+        }
+    }
+}
