078b8a900b83e9afe3b4dc5cda28ea698ab7624d
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2023-09-29 16:41:08 -0600

    92. Load Shopping Cart in Web Project

---
+++ Mango.Web/Controllers/CartController.cs
@@ -0,0 +1,42 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace Mango.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View();
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?
+                .FirstOrDefault()?.Value;
+            ResponseDto? responseDto = await _cartService.GetCartAsync(userId);
+
+            if (responseDto != null & responseDto.IsSuccessful)
+            {
+                CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(responseDto.Result.ToString());
+                return cartDto;
+            }
+
+            return new CartDto();
+        }
+    }
+}
